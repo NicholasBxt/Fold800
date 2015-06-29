f3d450b785995ec3878399642f610f05bc117c9e
jock <jock@JockdeMacBook-Pro.local>
2015-06-29 21:07:32 +0800

    Initial Commit

---
+++ Fold800Tests/Fold800Tests.m
@@ -0,0 +1,40 @@
+//
+//  Fold800Tests.m
+//  Fold800Tests
+//
+//  Created by jock on 15/6/29.
+//  Copyright (c) 2015年 jock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Fold800Tests : XCTestCase
+
+@end
+
+@implementation Fold800Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
